22b24ab7ab8bc6340466bdaafc3567d35f53539c
saad sufyan <saad.sufyan@loopsdigital.com>
2019-11-22 01:53:02 +0500

    Fix build issue

---
+++ build/precache-manifest.ce1e0fa7ee12babb01b1c41dd38039d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9613f0ef1713d050a89",
+    "url": "/static/js/main.a9613f0e.chunk.js"
+  },
+  {
+    "revision": "776ce8cdc119ac4bbab8",
+    "url": "/static/js/1.776ce8cd.chunk.js"
+  },
+  {
+    "revision": "a9613f0ef1713d050a89",
+    "url": "/static/css/main.20d1a622.chunk.css"
+  },
+  {
+    "revision": "5f056fbf0e14ed533ab3883181690699",
+    "url": "/index.html"
+  }
+];
